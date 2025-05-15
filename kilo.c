9a707d2a54b91439cab90f388753562b8957259e
Jam555 <3349478+jam555@users.noreply.github.com>
2025-05-15 06:39:01 -0500

    Break escape codes in editorRefreshScreen() into their own funcs.
    Signed-off-by: Jam555 <3349478+jam555@users.noreply.github.com>

--- kilo.c
+++ kilo.c
@@ -157,8 +157,8 @@ void mila_term_cursseek_setpos( int alter, int ofile, int row, int col );
 int mila_term_cursseek_finalchar( int alter );
 
 void mila_term_altscreen_disable( void );
-/* TODO: We've replaced three explicit escape-code emissions with wrapper */
-/*  functions, continue on to do most or all of the others. */
+
+void mila_ab_curvis_hide( void );
 
 
 
@@ -1022,6 +1022,88 @@ void abFree(struct abuf *ab) {
     free(ab->b);
 }
 
+void mila_ab_curseek( struct abuf *ab, int argn,   int x, int y, char *tail )
+{
+	char buf[32];
+	
+#define MILA_TERMCODES_8 "\x1b[H%s" /* Go home. */
+#define mila_ab_curseek_ONEARG "\x1b[%dH%s"
+#define MILA_TERMCODES_21 "\x1b[%d;%dH%s"
+	
+	if( !tail )
+	{
+		tail = "";
+	}
+	
+	/* Populate the buffer. */
+	if( argn == 0 )
+	{
+		snprintf( buf, sizeof(buf), MILA_TERMCODES_8,  tail );
+		
+	} else if( argn == 1 )
+	{
+		snprintf( buf, sizeof(buf), mila_ab_curseek_ONEARG, y,  tail );
+		
+	} else if( argn == 2 )
+	{
+		snprintf (buf, sizeof(buf), MILA_TERMCODES_21, y, x,  tail );
+	}
+	
+	abAppend( ab,buf,strlen(buf));
+}
+void mila_ab_curvis_hide( struct abuf *ab )
+{
+#define MILA_TERMCODES_7 "\x1b[?25l"
+    abAppend( ab,MILA_TERMCODES_7,6); /* Hide cursor. */
+}
+void mila_ab_curvis_show( struct abuf *ab )
+{
+#define MILA_TERMCODES_22 "\x1b[?25h"
+    abAppend( ab,MILA_TERMCODES_22,6); /* Show cursor. */
+}
+void mila_ab_curseek_home( struct abuf *ab )
+{
+	void mila_ab_curseek( ab, 0,   0, 0, "" );
+}
+void mila_ab_clearall( struct abuf *ab )
+{
+#define mila_ab_clearall_TERMCODE "~\x1b[2K\r\n"
+	abAppend( ab,mila_ab_clearall_TERMCODE,7);
+}
+void mila_ab_cleartostart( struct abuf *ab )
+{
+#define mila_ab_cleartostart_TERMCODE "~\x1b[1K\r\n"
+	abAppend( ab,mila_ab_cleartostart_TERMCODE,7);
+}
+void mila_ab_cleartoend( struct abuf *ab, chat *tail )
+{
+	char buf[32];
+	
+	if( !tail )
+	{
+		tail = "";
+	}
+	
+#define mila_ab_cleartoend_TERMCODE "~\x1b[0K%s"
+	snprintf( buf, sizeof(buf), mila_ab_cleartoend_TERMCODE,  tail );
+	
+	abAppend( ab,buf,strlen(buf));
+}
+void mila_ab_swapFgBg( struct abuf *ab )
+{
+#define MILA_TERMCODES_11 "\x1b[7m"
+	abAppend( ab,MILA_TERMCODES_11,4);
+}
+void mila_ab_resetAttribs( struct abuf *ab )
+{
+#define MILA_TERMCODES_12 "\x1b[0m"
+	abAppend( ab,MILA_TERMCODES_12,4);
+}
+void mila_ab_defaultFg( struct abuf *ab )
+{
+#define MILA_TERMCODES_13 "\x1b[39m"
+	abAppend( ab,MILA_TERMCODES_13,5);
+}
 /* This function writes the whole screen using VT100 escape characters
  * starting from the logical state of the editor in the global state 'E'. */
 void editorRefreshScreen(void) {
@@ -1030,10 +1112,8 @@ void editorRefreshScreen(void) {
     char buf[32];
     struct abuf ab = ABUF_INIT;
 
-#define MILA_TERMCODES_7 "\x1b[?25l"
-    abAppend(&ab,MILA_TERMCODES_7,6); /* Hide cursor. */
-#define MILA_TERMCODES_8 "\x1b[H"
-    abAppend(&ab,MILA_TERMCODES_8,3); /* Go home. */
+    mila_ab_curvis_hide( &ab );
+    mila_ab_curseek_home( &ab );
     for (y = 0; y < E.screenrows; y++) {
         int filerow = E.rowoff+y;
 
@@ -1045,14 +1125,13 @@ void editorRefreshScreen(void) {
                     "Kilo editor -- verison %s%s\r\n", KILO_VERSION,MILA_TERMCODES_9);
                 int padding = (E.screencols-welcomelen)/2;
                 if (padding) {
-                    abAppend(&ab,"~",1);
+                    abAppend( &ab,"~",1);
                     padding--;
                 }
                 while(padding--) abAppend(&ab," ",1);
-                abAppend(&ab,welcome,welcomelen);
+                abAppend( &ab,welcome,welcomelen);
             } else {
-#define MILA_TERMCODES_10 "~\x1b[0K\r\n"
-                abAppend(&ab,MILA_TERMCODES_10,7);
+                mila_ab_cleartoend(  &ab, "\r\n" );
             }
             continue;
         }
@@ -1069,41 +1148,35 @@ void editorRefreshScreen(void) {
             for (j = 0; j < len; j++) {
                 if (hl[j] == HL_NONPRINT) {
                     char sym;
-#define MILA_TERMCODES_11 "\x1b[7m"
-                    abAppend(&ab,MILA_TERMCODES_11,4);
+                    mila_ab_swapFgBg( &ab );
                     if (c[j] <= 26)
                         sym = '@'+c[j];
                     else
                         sym = '?';
-                    abAppend(&ab,&sym,1);
-#define MILA_TERMCODES_12 "\x1b[0m"
-                    abAppend(&ab,MILA_TERMCODES_12,4);
+                    abAppend( &ab,&sym,1);
+                    mila_ab_resetAttribs( &ab );
                 } else if (hl[j] == HL_NORMAL) {
-                    if (current_color != -1) {
-#define MILA_TERMCODES_13 "\x1b[39m"
-                        abAppend(&ab,MILA_TERMCODES_13,5);
+                    if( current_color != -1 ) {
+                        mila_ab_defaultFg( &ab );
                         current_color = -1;
                     }
-                    abAppend(&ab,c+j,1);
+                    abAppend( &ab,c+j,1);
                 } else {
-                    int color = editorSyntaxToColor(hl[j]);
+                    int color = editorSyntaxToColor( hl[j] );
                     if (color != current_color) {
                         char buf[16];
 #warning "We can't do this, we need to print ints!"
 #define MILA_TERMCODES_14 "\x1b[%dm"
-                        int clen = snprintf(buf,sizeof(buf),MILA_TERMCODES_14,color);
+                        int clen = snprintf( buf,sizeof(buf),MILA_TERMCODES_14,color );
                         current_color = color;
-                        abAppend(&ab,buf,clen);
+                        abAppend( &ab,buf,clen );
                     }
-                    abAppend(&ab,c+j,1);
+                    abAppend( &ab,c+j,1 );
                 }
             }
         }
-#define MILA_TERMCODES_15 "\x1b[39m"
-        abAppend(&ab,MILA_TERMCODES_15,5);
-#define MILA_TERMCODES_16 "\x1b[0K"
-        abAppend(&ab,MILA_TERMCODES_16,4);
-        abAppend(&ab,"\r\n",2);
+        mila_ab_defaultFg( &ab );
+        mila_ab_cleartoend( &ab, "\r\n" );
     }
 
 
@@ -1115,35 +1188,44 @@ void editorRefreshScreen(void) {
 
 
     /* Create a two rows status. First row: */
-#define MILA_TERMCODES_17 "\x1b[0K"
-    abAppend(&ab,MILA_TERMCODES_17,4);
-#define MILA_TERMCODES_18 "\x1b[7m"
-    abAppend(&ab,MILA_TERMCODES_18,4);
+    mila_ab_cleartoend( &ab, "" );
+	mila_ab_swapFgBg( &ab );
     char status[80], rstatus[80];
-    int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
-        E.filename, E.numrows, E.dirty ? "(modified)" : "");
-    int rlen = snprintf(rstatus, sizeof(rstatus),
-        "%d : %d/%d",E.cx+1,E.rowoff+E.cy+1,E.numrows);
-    if (len > E.screencols) len = E.screencols;
-    abAppend(&ab,status,len);
-    while(len < E.screencols) {
-        if (E.screencols - len == rlen) {
-            abAppend(&ab,rstatus,rlen);
+	int len =
+		snprintf
+		(
+			status, sizeof( status ),
+			"%.20s - %d lines %s",  E.filename, E.numrows, E.dirty ? "(modified)" : ""
+		);
+    int rlen =
+		snprintf
+		(
+			rstatus, sizeof(rstatus),
+			"%d : %d/%d",  E.cx+1, E.rowoff+E.cy+1, E.numrows
+		);
+    if( len > E.screencols )
+	{
+		len = E.screencols;
+	}
+    abAppend( &ab,status,len );
+    while( len < E.screencols ) {
+        if( E.screencols - len == rlen ) {
+            abAppend( &ab, rstatus, rlen );
             break;
         } else {
-            abAppend(&ab," ",1);
+            abAppend( &ab, " ", 1 );
             len++;
         }
     }
 #define MILA_TERMCODES_19 "\x1b[0m\r\n"
-    abAppend(&ab,MILA_TERMCODES_19,6);
+		/* This would be mila_ab_resetAttribs(), but that DOESN'T include newlines. */
+    abAppend( &ab,MILA_TERMCODES_19,6 );
 
     /* Second row depends on E.statusmsg and the status message update time. */
-#define MILA_TERMCODES_20 "\x1b[0K"
-    abAppend(&ab,MILA_TERMCODES_20,4);
-    int msglen = strlen(E.statusmsg);
-    if (msglen && time(NULL)-E.statusmsg_time < 5)
-        abAppend(&ab,E.statusmsg,msglen <= E.screencols ? msglen : E.screencols);
+    mila_ab_cleartoend( &ab, "" );
+    int msglen = strlen( E.statusmsg );
+    if( msglen && time(NULL)-E.statusmsg_time < 5 )
+        abAppend( &ab, E.statusmsg, msglen <= E.screencols ? msglen : E.screencols );
 
     /* Put cursor at its current position. Note that the horizontal position
      * at which the cursor is displayed may be different compared to 'E.cx'
@@ -1155,30 +1237,32 @@ void editorRefreshScreen(void) {
     int cx = 1;
     int filerow = E.rowoff+E.cy;
     erow *row = (filerow >= E.numrows) ? NULL : &E.row[filerow];
-    if (row) {
-        for (j = E.coloff; j < (E.cx+E.coloff); j++) {
-            if (j < row->size && row->chars[j] == TAB) cx += (MILA_TABSIZE-1)-((cx)%MILA_TABSIZE);
+    if( row )
+	{
+        for( j = E.coloff; j < (E.cx+E.coloff); j++ )
+		{
+            if( j < row->size && row->chars[j] == TAB )
+			{
+				cx += (MILA_TABSIZE-1)-((cx)%MILA_TABSIZE);
+			}
             cx++;
         }
     }
-#warning "We can't do this, we need to print stuff!"
-#define MILA_TERMCODES_21 "\x1b[%d;%dH"
-    snprintf(buf,sizeof(buf),MILA_TERMCODES_21,E.cy+1,cx);
-    abAppend(&ab,buf,strlen(buf));
-#define MILA_TERMCODES_22 "\x1b[?25h"
-    abAppend(&ab,MILA_TERMCODES_22,6); /* Show cursor. */
+    mila_ab_curseek( &ab, 2,   E.cy+1,cx, "" ); /* Move cursor. */
+	mila_ab_curvis_show( &ab ); /* Show cursor. */
+	
     write(STDOUT_FILENO,ab.b,ab.len);
-    abFree(&ab);
+    abFree( &ab );
 }
 
 /* Set an editor status message for the second line of the status, at the
  * end of the screen. */
 void editorSetStatusMessage(const char *fmt, ...) {
     va_list ap;
-    va_start(ap,fmt);
-    vsnprintf(E.statusmsg,sizeof(E.statusmsg),fmt,ap);
-    va_end(ap);
-    E.statusmsg_time = time(NULL);
+    va_start( ap,fmt );
+    vsnprintf( E.statusmsg,sizeof(E.statusmsg),fmt,ap );
+    va_end( ap );
+    E.statusmsg_time = time( NULL );
 }
 
 /* =============================== Find mode ================================ */
