18db6685d920055142ff03e4647241c8cff41c8b
Jam555 <3349478+jam555@users.noreply.github.com>
2025-08-08 09:07:27 -0500

    Update project name in heading comment.
    Signed-off-by: Jam555 <3349478+jam555@users.noreply.github.com>

--- statview.c
+++ statview.c
@@ -1,4 +1,4 @@
-/* Mila -- A very simple editor derived from Salvatore Sanfilippo's Kilo,
+/* Thou -- A very simple editor derived from Salvatore Sanfilippo's Kilo,
  *         a text editor in less than 1-kilo lines of code (as counted
  *         by "cloc"). Does not depend on libcurses, directly emits VT100
  *         escapes on the terminal.
